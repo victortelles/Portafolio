078384eb8490473ad05ffc3cfa4b3b6b414bfdaa
VMLAP\VMLap <victor-tah@outlook.com>
2025-10-12 15:52:04 -0600

    add: nuevo tipado para footer

---
+++ src/types/footer.ts
@@ -0,0 +1,39 @@
+// Types for Footer Component
+
+// Footer Marca
+export type FooterBrand = {
+    name: string;
+    tagline: string;
+    avatar: string;
+};
+
+// Footer Redes Sociales
+export type FooterSocial = {
+    platform: string;
+    href: string;
+    label: string;
+};
+
+// Footer Enlaces Rápidos
+export type FooterQuickLink = {
+    label: string;
+    href: string;
+};
+
+// Estructura completa del contenido del Footer
+export type FooterContent = {
+    brand: FooterBrand;
+    socials: FooterSocial[];
+    quickLinks: FooterQuickLink[];
+    footer: {
+        copyright: string;
+        privacy: {
+            label: string;
+            href: string;
+        };
+        credits: {
+            label: string;
+            href: string;
+        };
+    };
+};

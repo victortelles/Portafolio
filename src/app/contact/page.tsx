dfad4d7123cc0718c00c7e04b71f4e4da979bf8d
VMLAP\VMLap <victor-tah@outlook.com>
2025-10-12 15:50:35 -0600

    add: nuevo componente de contacto

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+import type React from "react"
+import ContactPage from "@/components/contact/ContactPage/ContactPage"
+
+const Contact: React.FC = () => {
+    return <ContactPage />
+}
+
+export default Contact
